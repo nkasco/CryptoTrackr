c2913e4522026a8aae634cc9c346cb0b075d6b92
Nate Kasco <nkasco@gmail.com>
2018-03-03 17:27:27 -0500

    Added config

--- crypto-trackr.ps1
+++ crypto-trackr.ps1
@@ -4,8 +4,22 @@
 # Date Started: 2/24/2018                      #
 ################################################
 
+$ErrorActionPreference = "SilentlyContinue"
+
 #Install CoinMarketCap Module - Credit: https://github.com/lazywinadmin/CoinMarketCap
 if(!(Get-Module -Name CoinMarketCap)){
     Install-Module -ModulePath "CoinMarketCap\CoinMarketCap.psm1"
 }
 
+#Load config from AppData
+$ConfigLocation = "$env:LOCALAPPDATA\CryptoTrackr\config.csv"
+if(Test-Path $ConfigLocation){
+    $Config = Import-Csv $ConfigLocation
+} else {
+    #Build blank config file
+    if(!(Test-Path(Split-Path $ConfigLocation))){
+        New-Item (Split-Path $ConfigLocation) -ItemType Directory | Out-Null
+    }
+    @("Coins`nBTC`nETH`nLTC") | Out-File $ConfigLocation -Force
+    $Config = Import-Csv $ConfigLocation
+}
